da0813a768deba9a153cb5cd8efc24d535a12288
Ill-Come-Up-With-The-Name-Later <magidonovan@gmail.com>
2025-11-23 15:38:53 -0500

    Re-added the other players.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game();
         game.addPlayer(new Player("Player 1"));
-        
+        game.addPlayer(new Player("Player 2"));
+        game.addPlayer(new Player("Player 3"));
+        game.addPlayer(new Player("Player 4"));
+        game.addPlayer(new Player("Player 5"));
+        game.addPlayer(new Player("Player 6"));
+        game.addPlayer(new Player("Player 7"));
 
         GameManager gameManager = new GameManager(game);
         gameManager.runUntilCardsOut();
