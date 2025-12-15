120ea7fd9aaef5af8b0b141463c2016ff11c1c0c
Ill-Come-Up-With-The-Name-Later <adtmax5@gmail.com>
2025-12-15 15:36:42 -0500

    Something

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game();
 
-        for(int i = 0; i < 7; i++) {
+        for(int i = 0; i < 10; i++) {
             Player player = new Player("Player " + (i + 1), 1000);
             game.addPlayer(player);
         }
